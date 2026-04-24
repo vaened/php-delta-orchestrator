0b96bc76104e7ed7bd4a08f6cb028e6952a8ff9a
Enea Dhack <enea@vaened.dev>
2026-04-24 18:07:50 -0500

    feat: add initial classes for DeltaOrchestrator
    Introduced several new classes and interfaces for the DeltaOrchestrator module. This includes Action, Input, Behavior, Comparator, Delta, Field, Optional, Orchestrator, Required, Result, and Schema.

---
+++ src/Bindings/Behavior.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+declare(strict_types=1);
+
+namespace Vaened\DeltaOrchestrator\Bindings;
+
+interface Behavior
+{
+}
