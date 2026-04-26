9d03f6cea214a5941b57932c77c755f89469c0d5
Enea Dhack <enea@vaened.dev>
2026-04-26 17:18:47 -0500

    feat: add PHPUnit configuration and base test case class
    Introduced a PHPUnit configuration file to define test suites and source directories. Added an abstract base test case class extending PHPUnit's TestCase for consistent testing practices.

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author enea dhack <contact@vaened.dev>
+ * @link https://vaened.dev DevFolio
+ */
+
+declare(strict_types=1);
+
+namespace Vaened\DeltaOrchestrator\Tests;
+
+use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+
+abstract class TestCase extends PHPUnitTestCase
+{
+}
