09ec9edeb1c16801a11675d51050b1a7ff39bd01
Enea Dhack <enea@vaened.dev>
2026-04-25 21:40:24 -0500

    feat: add constructor and methods for fields and closures in Action class
    Introduced a constructor to initialize fields, apply, and when closures. Added methods to retrieve fields and closures for further processing.

--- src/Action.php
+++ src/Action.php
@@ -8,6 +8,40 @@ declare(strict_types=1);
 
 namespace Vaened\DeltaOrchestrator;
 
+use Closure;
+use Vaened\DeltaOrchestrator\Bindings\Behavior;
+use Vaened\DeltaOrchestrator\Rules\Rule;
+
 final readonly class Action
 {
+    /**
+     * @param array<int, Field|Behavior> $fields
+     * @param Closure(Field ...$fields): Rule|null $when
+     * @param Closure(Field ...$fields): mixed $apply
+     */
+    public function __construct(
+        private array    $fields,
+        private Closure  $apply,
+        private ?Closure $when = null,
+    )
+    {
+    }
+
+    /**
+     * @return array<int, Field|Behavior>
+     */
+    public function fields(): array
+    {
+        return $this->fields;
+    }
+
+    public function apply(): Closure
+    {
+        return $this->apply;
+    }
+
+    public function when(): ?Closure
+    {
+        return $this->when;
+    }
 }
